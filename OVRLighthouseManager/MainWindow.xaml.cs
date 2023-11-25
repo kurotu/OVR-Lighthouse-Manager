8c0b04bf13584b845f94d8e33dc7198a5c8bee67
kurotu <kurotu@gmail.com>
2023-11-25 20:59:06 +0900

    Resize window on startup

--- OVRLighthouseManager/MainWindow.xaml.cs
+++ OVRLighthouseManager/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ public sealed partial class MainWindow : WindowEx
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
+        AppWindow.Resize(new Windows.Graphics.SizeInt32(500, 500));
 
         // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
         dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
