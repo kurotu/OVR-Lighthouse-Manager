9e7cc97aae426145ea7af9511f4a9605ffde3215
kurotu <kurotu@gmail.com>
2023-11-30 23:11:17 +0900

    Add setup custom actions

---
+++ OVRLighthouseManagerSetupActions/CustomActions.cs
@@ -0,0 +1,16 @@
+﻿namespace OVRLighthouseManagerSetupActions
+{
+    [System.ComponentModel.RunInstaller(true)]
+    public class CustomActions : System.Configuration.Install.Installer
+    {
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
