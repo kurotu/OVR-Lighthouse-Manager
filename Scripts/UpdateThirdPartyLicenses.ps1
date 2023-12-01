e2ba2b41d8b30df0b0df577e4083e71b8208c100
kurotu <kurotu@gmail.com>
2023-12-01 15:26:06 +0900

    Add third party licenses list

---
+++ Scripts/UpdateThirdPartyLicenses.ps1
@@ -0,0 +1,4 @@
+dotnet dotnet-project-licenses -i OVRLighthouseManager -t --use-project-assets-json -m
+echo "OVR Lighthouse Manager uses the following third party libraries:" > ThirdPartyLicenses.md
+cat licenses.md >> ThirdPartyLicenses.md
+rm licenses.md
