a8350ce6b4015174a3a6923c0c18b395a3a66508
kurotu <kurotu@gmail.com>
2024-02-18 20:59:30 +0900

    Update changelog in bump.sh

--- Scripts/bump.sh
+++ Scripts/bump.sh
@@ -3,9 +3,10 @@ set -eu
 
 VERSION=${1}
 
+sed -i -b -e "s/\[Unreleased\]/\[${VERSION}\] - $(date -I)/g" CHANGELOG*.md
 sed -i -b -e "s/#define MyAppVersion \".*\"/#define MyAppVersion \"${VERSION}\"/g" Installer/Installer.iss
 sed -i -b -e "s/<Version>.*<\/Version>/<Version>${VERSION}<\/Version>/g" OVRLighthouseManager/OVRLighthouseManager.csproj
 
-git add Installer/Installer.iss OVRLighthouseManager/OVRLighthouseManager.csproj
+git add CHANGELOG*.md Installer/Installer.iss OVRLighthouseManager/OVRLighthouseManager.csproj
 git commit -m "Version ${VERSION}"
 git tag "${VERSION}"
